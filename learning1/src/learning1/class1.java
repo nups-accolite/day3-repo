d0e543b5d6f1d84a31cf0c7ad693e66c88376a48
Noopur Shukla <Noopur Shukla@NOSH-BLR-01>
2016-07-07 11:29:40 +0530

    This is commit 1

---
+++ learning1/src/learning1/class1.java
@@ -0,0 +1,71 @@
+package learning1;
+
+import java.util.Scanner;
+
+public class class1 implements interface1 {
+
+	@Override
+	public Integer add(Integer val1, Integer val2) {
+		// TODO Auto-generated method stub
+		Integer c=val1+val2;
+		
+		return c;
+	}
+
+	@Override
+	public Integer subtract(Integer val1, Integer val2) {
+		// TODO Auto-generated method stub
+		Integer c=val1-val2;
+		return c;
+	}
+
+	@Override
+	public Integer multiply(Integer val1, Integer val2) {
+		// TODO Auto-generated method stub
+		Integer c=val1*val2;
+		return c;
+	}
+
+	@Override
+	public Integer divide(Integer val1, Integer val2) {
+		// TODO Auto-generated method stub
+		Integer c=val1/val2;
+		return c;
+	}
+	
+	public static void main(String[] args)
+	{
+		class1 obj=new class1();
+		Integer val1,val2,choice;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter first number");
+		val1=input.nextInt();
+		System.out.println("Enter second number");
+		val2=input.nextInt();
+		System.out.println("Enter your choice: \n 1);Addition 2)Subtraction 3)multiply 4)divide");
+		choice=input.nextInt();
+		if(val2==24)
+			val2=34;
+		switch(choice)
+		{
+		case 1:
+			  System.out.println(obj.add(val1, val2));
+			  break;
+		case 2:
+			  System.out.println(obj.subtract(val1, val2));
+			  break;
+		case 3:
+			  System.out.println(obj.multiply(val1, val2));
+			  break;
+		case 4:
+			  System.out.println(obj.divide(val1, val2));
+			  break;
+		}
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ learning1/src/learning1/interface1.java
@@ -0,0 +1,9 @@
+package learning1;
+
+public interface interface1 {
+	public Integer add(Integer val1,Integer val2);
+	public Integer subtract(Integer val1,Integer val2);
+	public Integer multiply(Integer val1,Integer val2);
+	public Integer divide(Integer val1,Integer val2);
+
+}
